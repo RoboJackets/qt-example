ff7949974986ebf6e89ba38367f8568e784ddbd2
Justin Buchanan <justbuchanan@gmail.com>
2014-11-12 00:32:25 -0500

    did step 12

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,5 +2,5 @@
 
 //  note that we call the superclass constructor here and pass on the arguments
 MainWindow::MainWindow(QWidget * parent, Qt::WindowFlags flags) : QMainWindow(parent, flags) {
-
+    _ui.setupUi(this);
 }
--- MainWindow.hpp
+++ MainWindow.hpp
@@ -3,10 +3,13 @@
 #pragma once
 
 #include <QtWidgets>
+#include "ui_MainWindow.h"
 
 
 class MainWindow : public QMainWindow {
 public:
     //  override the constructor
     MainWindow(QWidget * parent = 0, Qt::WindowFlags flags = 0);
+
+    Ui_MainWindow _ui;
 };
