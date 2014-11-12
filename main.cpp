06dba4a8bae20f89fcfe54237a9594a5fcb36b6b
Justin Buchanan <justbuchanan@gmail.com>
2014-11-11 21:53:31 -0500

    did step 5

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 //  the iostream header contains cout among other things
 #include <iostream>
 
+#include <QtWidgets>
+
 //  this declaration lets us leave of the "std::" when using things in the standard libraries
 //  without it, we'd have to type "std::cout", not we can just type "cout"
 using namespace std;
 
 
 int main(int argc, char **argv) {
-    cout << "Hello World" << endl;
+    QApplication app(argc, argv);
 
-    //  return 0 to indicate success
-    return 0;
+    return app.exec();
 }
